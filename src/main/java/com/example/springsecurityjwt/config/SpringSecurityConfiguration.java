76841db2fbc86833e895e6575a92d66c74fec083
Mushegh <asddsajpg@gmail.com>
2021-03-30 21:41:21 +0400

    simple jwt authentication

--- src/main/java/com/example/springsecurityjwt/config/SpringSecurityConfiguration.java
+++ src/main/java/com/example/springsecurityjwt/config/SpringSecurityConfiguration.java
@@ -3,12 +3,15 @@ package com.example.springsecurityjwt.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
@@ -16,6 +19,12 @@ public class SpringSecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Autowired
     private CustomUserDetailService userDetailsService;
+
+    @Autowired
+    private CustomJwtAuthenticationFilter customJwtAuthenticationFilter;
+
+    @Autowired
+    private JwtAuthenticationEntryPoint unauthorizedHandler;
 
     @Bean
     public PasswordEncoder passwordEncoder() {
@@ -27,15 +36,25 @@ public class SpringSecurityConfiguration extends WebSecurityConfigurerAdapter {
         auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder());
     }
 
+    @Bean
+    @Override
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
+    }
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .csrf().disable()
-                .authorizeRequests().antMatchers("/helloamin")
-                .hasRole("ADMIN")
-                .antMatchers("/hellouser")
-                .hasAnyRole("ADMIN", "USER")
+                .authorizeRequests().antMatchers("/helloamin").hasRole("ADMIN")
+                .antMatchers("/hellouser").hasAnyRole("ADMIN", "USER")
+                .antMatchers("/authenticate").permitAll().anyRequest().authenticated()
+                .and().exceptionHandling()
+                .authenticationEntryPoint(unauthorizedHandler)
                 .and()
-                .httpBasic();
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+
+        http
+                .addFilterBefore(customJwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
     }
 }
